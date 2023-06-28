635668c7ba2c85a4a7a90e6b316488eb7f87e354
CaptSiro <honzahorak433@icloud.com>
2023-06-28 14:49:18 +0200

    Imported missing rust library utils

---
+++ src/renderer/src/lib/rust-like-utils-client/Optional.ts
@@ -0,0 +1,16 @@
+import { Optional } from '../../../../@types';
+
+
+
+export function none(): Optional<any> {
+    return {
+      isNone: true
+    };
+}
+
+export function some<V>(value: V): Optional<V> {
+    return {
+        value,
+        isNone: false
+    };
+}
