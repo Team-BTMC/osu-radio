af2419c1c9649e2ea3ce253cbaaa5433fe2c5735
CaptSiro <honzahorak433@icloud.com>
2023-07-01 14:12:08 +0200

    Added orDefault function to Optional.ts

--- src/renderer/src/lib/rust-like-utils-client/Optional.ts
+++ src/renderer/src/lib/rust-like-utils-client/Optional.ts
@@ -3,14 +3,22 @@ import { Optional } from '../../../../@types';
 
 
 export function none(): Optional<any> {
-    return {
-      isNone: true
-    };
+  return {
+    isNone: true
+  };
 }
 
 export function some<V>(value: V): Optional<V> {
-    return {
-        value,
-        isNone: false
-    };
+  return {
+    value,
+    isNone: false
+  };
+}
+
+export function orDefault<V>(opt: Optional<V>, def: V): V {
+  if (opt.isNone) {
+    return def;
+  }
+
+  return opt.value;
 }
