75e1f0112265f63530b0557f8713314f38bb1927
CaptSiro <honzahorak433@icloud.com>
2023-07-01 15:26:30 +0200

    ICANT Using the wrong variable for guard class KEKW

--- src/renderer/src/lib/Dispatcher.ts
+++ src/renderer/src/lib/Dispatcher.ts
@@ -1,31 +1,33 @@
 export type EmitterListener<Args> = (args?: Args) => any;
 
+
+
 /**
  * @template EventMap Represents a map of possible events with argument tuple: { "mute": [boolean] }
  */
 export class Dispatcher<EventMap> {
-    #listeners: Map<keyof EventMap, EmitterListener<any>[]> = new Map();
+  #listeners: Map<keyof EventMap, EmitterListener<any>[]> = new Map();
 
-    on<E extends keyof EventMap>(event: E, listener: EmitterListener<EventMap[E]>): void {
-        const listeners = this.#listeners.get(event);
+  on<E extends keyof EventMap>(event: E, listener: EmitterListener<EventMap[E]>): void {
+    const listeners = this.#listeners.get(event);
 
-        if (listeners === undefined) {
-            this.#listeners.set(event, [listener]);
-            return;
-        }
-
-        listeners.push(listener);
+    if (listeners === undefined) {
+      this.#listeners.set(event, [listener]);
+      return;
     }
 
-    dispatch<E extends keyof EventMap>(event: E, args: EventMap[E]): void {
-        const listeners = this.#listeners.get(event);
+    listeners.push(listener);
+  }
 
-        if (event === undefined) {
-            return;
-        }
+  dispatch<E extends keyof EventMap>(event: E, args: EventMap[E]): void {
+    const listeners = this.#listeners.get(event);
 
-        for (let i = 0; i < listeners.length; i++) {
-            listeners[i](args);
-        }
+    if (listeners === undefined) {
+      return;
     }
+
+    for (let i = 0; i < listeners.length; i++) {
+      listeners[i](args);
+    }
+  }
 }
