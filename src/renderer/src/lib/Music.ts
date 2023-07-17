d43559d6777ef044fef18eec05a5aaa4188d94f6
CaptSiro <honzahorak433@icloud.com>
2023-07-17 02:01:21 +0200

    Version 1 of redesigned Music.ts

--- src/renderer/src/lib/Music.ts
+++ src/renderer/src/lib/Music.ts
@@ -1,27 +1,7 @@
-import { Optional, Song } from "../../@types";
-import { Dispatcher } from "./Dispatcher.js";
 import { none, some } from "./rust-like-utils-client/Optional.js";
-import { importAPI } from "./tungsten/api";
-
-
-
-const apiOptional = importAPI();
-if (apiOptional.isNone) {
-    throw new Error("Chief, we've got a problem. No API found to be fuc- I mean... use :D");
-}
-const api = apiOptional.value;
-
-const player = new Audio();
-player.volume = 0.5;
-
-
-
-const current: {
-    media?: URL,
-    song?: Song,
-    bpm?: number
-} = {};
-let isPlaying: boolean = false;
+import { Optional, Song } from '../../../@types';
+import { msToBPM } from './song';
+import { createEffect, createSignal } from 'solid-js';
 
 
 
@@ -29,128 +9,192 @@ type ZeroToOne = number;
 
 
 
-export class Music {
-    static readonly events: Dispatcher<{
-        "songChange": Song,
-        "timeUpdate": {
-            current: number,
-            duration: number
-        },
-        "bpmUpdate": number | undefined
-    }> = new Dispatcher();
+const player = new Audio();
+player.volume = 0.3;
 
 
 
-    static async current(): Promise<URL> {
-        current.song = await api.request("queueCurrent");
-        current.media = new URL(current.song.dir + "\\" + current.song.audio.fileName);
-        return current.media;
+const [media, setMedia] = createSignal<URL>();
+const [song, setSong] = createSignal<Song | undefined>(undefined, {
+  equals: (prev, next) => {
+    return JSON.stringify(prev) !== JSON.stringify(next);
+  }
+});
+const [duration, setDuration] = createSignal(0);
+const [timestamp, setTimestamp] = createSignal(0);
+const [bpm, setBPM] = createSignal<Optional<number>>(none(), {
+  equals: (prev, next) => {
+    if (prev.isNone && !next.isNone) {
+      return true;
     }
 
-    static async play(): Promise<void> {
-        if (current.media === undefined) {
-            await Music.current();
-        }
-
-        if (player.src !== current.media.href) {
-            player.src = current.media.href;
-            this.events.dispatch("songChange", current.song);
-        }
-
-        player.volume = 0.5;
-        await player.play();
+    if (!prev.isNone && next.isNone) {
+      return true;
     }
 
-    static pause(): void {
-        player.pause();
+    if (!prev.isNone && !next.isNone) {
+      return prev.value !== next.value;
     }
 
-    static async next(): Promise<void> {
-        await api.request("queueNext");
-        player.src = (await this.current()).href;
+    return true;
+  }
+});
+const [isPlaying, setIsPlaying] = createSignal<boolean>(false);
 
-        if (isPlaying === true) {
-            await this.play();
-        }
+export { isPlaying, bpm, song, media, duration, timestamp }
 
-        this.events.dispatch("songChange", current.song);
-    }
 
-    static async previous(): Promise<void> {
-        await api.request("queuePrevious");
-        player.src = (await this.current()).href;
 
-        if (isPlaying === true) {
-            await this.play();
-        }
+async function getCurrent() {
+  const song = await window.api.request("queueCurrent");
+  const resource = await window.api.request("resourceGet", song.audio);
 
-        this.events.dispatch("songChange", current.song);
-    }
+  if (resource.isError) {
+    return;
+  }
 
-    static async togglePlay(force?: boolean): Promise<void> {
-        if (arguments.length === 1) {
-            isPlaying = force;
+  const media = new URL(resource.value);
 
-            if (force === true) {
-                await Music.play();
-                return;
-            }
-
-            Music.pause();
-            return;
-        }
-
-        if (isPlaying === true) {
-            isPlaying = !isPlaying;
-            Music.pause();
-            return;
-        }
-
-        isPlaying = !isPlaying;
-        await Music.play();
-    }
-
-    static isPlaying(): boolean {
-        return isPlaying;
-    }
-
-    static seek(range: ZeroToOne): void {
-        player.currentTime = range * player.duration;
-        this.events.dispatch("timeUpdate", {
-            current: player.currentTime,
-            duration: player.duration
-        });
-    }
+  return {
+    song,
+    media
+  };
 }
-Music.events.on("songChange", () => {
-    current.bpm = undefined;
-    Music.events.dispatch("bpmUpdate", undefined);
+
+export async function play(): Promise<void> {
+  if (media() === undefined) {
+    const current = await getCurrent();
+    if (current === undefined) {
+      console.error("Cannot play current song");
+      return;
+    }
+
+    setSong(current.song);
+    setMedia(current.media);
+  }
+
+  const m = media();
+
+  if (m !== undefined && player.src !== m.href) {
+    player.src = m.href;
+  }
+
+  player.volume = 0.3;
+  await player.play();
+}
+
+export async function next() {
+  await window.api.request("queueNext");
+
+  const current = await getCurrent();
+  if (current === undefined) {
+    console.error("Can't forward queue");
+    return;
+  }
+
+  player.src = current.media.href;
+  setMedia(current.media);
+
+  if (isPlaying() === true) {
+    await play();
+  }
+
+  setSong(current.song);
+}
+
+export async function previous() {
+  await window.api.request("queuePrevious");
+
+  const current = await getCurrent();
+  if (current === undefined) {
+    console.error("Can't rollback queue");
+    return;
+  }
+
+  player.src = current.media.href;
+  setMedia(current.media);
+
+  if (isPlaying() === true) {
+    await play();
+  }
+
+  setSong(current.song);
+}
+
+export async function togglePlay(force?: boolean): Promise<void> {
+  if (force !== undefined) {
+    setIsPlaying(force);
+
+    if (force === true) {
+      await play();
+      return;
+    }
+
+    player.pause();
+    return;
+  }
+
+  if (isPlaying() === true) {
+    setIsPlaying(!isPlaying());
+    player.pause();
+    return;
+  }
+
+  setIsPlaying(!isPlaying());
+  await play();
+}
+
+export function seek(range: ZeroToOne): void {
+  player.currentTime = range * player.duration;
+
+  setDuration(player.duration);
+  setTimestamp(player.currentTime);
+}
+
+
+
+createEffect(() => {
+  song();
+  setBPM(none());
+});
+
+
+
+window.api.listen("queueIndexMoved", async (song) => {
+  const resource = await window.api.request("resourceGet", song.audio);
+
+  if (resource.isError) {
+    return;
+  }
+
+  setMedia(new URL(resource.value));
 });
 
 
 
 player.addEventListener("ended", async () => {
-    await Music.next();
+    await next();
 });
 
 const OFFSET = 0;
 const BPM = 1;
 
 player.addEventListener("timeupdate", () => {
-    Music.events.dispatch("timeUpdate", {
-        current: player.currentTime,
-        duration: player.duration
-    });
+  setTimestamp(player.currentTime);
+  setDuration(player.duration);
 
-    if (current.song.bpm[0][OFFSET] / 1000 > player.currentTime) {
-        return;
-    }
+  const s = song();
 
-    const bpm = currentBPM(player.currentTime, current.song.bpm);
-    if (!bpm.isNone && bpm.value !== current.bpm) {
-        current.bpm = bpm.value;
-        Music.events.dispatch("bpmUpdate", bpm.value);
-    }
+  if (s === undefined || s.bpm[0][OFFSET] / 1000 > player.currentTime) {
+    return;
+  }
+
+  const bpmOpt = currentBPM(player.currentTime, s.bpm);
+  const current = bpm();
+
+  if (!bpmOpt.isNone && !current.isNone && bpmOpt.value !== current.value) {
+    setBPM(some(bpmOpt.value));
+  }
 });
 
 
@@ -162,19 +206,9 @@ function currentBPM(offset: number, changes: number[][]): Optional<number> {
 
     for (let i = 1; i < changes.length; i++) {
         if (changes[0][OFFSET] / 1000 <= offset && offset < changes[i][OFFSET] / 1000) {
-            return some(bpm(changes[i - 1][BPM]));
+            return some(msToBPM(changes[i - 1][BPM]));
         }
     }
 
-    return some(bpm(changes[changes.length - 1][BPM]));
-}
-
-
-
-function bpm(ms: number): number {
-    if (ms === 0 || 60_000 / ms > 10_000) {
-        return Infinity;
-    }
-
-    return Math.round(60_000 / ms);
+    return some(msToBPM(changes[changes.length - 1][BPM]));
 }
