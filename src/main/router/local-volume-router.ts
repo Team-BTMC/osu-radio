393bcc50d4e8081b39158c564cc0ef4ebee55166
CaptSiro <honzahorak433@icloud.com>
2023-07-18 15:29:33 +0200

    Delaying when writing audio source

---
+++ src/main/router/local-volume-router.ts
@@ -0,0 +1,27 @@
+import { Router } from '../lib/route-pass/Router';
+import { Storage } from '../lib/storage/Storage';
+import { delay } from '../lib/delay-backend';
+import { AudioSource, ResourceID } from '../../@types';
+
+
+
+const [writeAudioSource, ] = delay((audioID: ResourceID, audioSource: AudioSource) => {
+  Storage.getTable("audio").write(audioID, audioSource);
+}, 100);
+
+Router.respond("saveLocalVolume", (_evt, localVolume, songID) => {
+  const song = Storage.getTable("songs").get(songID);
+
+  if (song.isNone) {
+    return;
+  }
+
+  const audio = Storage.getTable("audio").get(song.value.audio);
+
+  if (audio.isNone) {
+    return;
+  }
+
+  audio.value.volume = localVolume;
+  writeAudioSource(song.value.audio, audio.value);
+});
