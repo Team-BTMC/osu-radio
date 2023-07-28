dc87097e2e4eb10c44993f47cd6408d502afe21a
CaptSiro <honzahorak433@icloud.com>
2023-07-28 19:59:07 +0200

    Updated to point to correct event

--- src/renderer/src/keyboard-registers/f2-shuffle.ts
+++ src/renderer/src/keyboard-registers/f2-shuffle.ts
@@ -5,7 +5,7 @@ import { Keyboard } from '../lib/Keyboard';
 Keyboard.register({
   key: "F2",
   onPress: async () => {
-    await window.api.request("queue.shuffle");
+    await window.api.request("queue::shuffle");
 
     //todo notify: 'Shuffled'
   }
