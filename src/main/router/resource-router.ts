680297f13c40187bd5cab86ef8ebf5faa7ea97b2
Plextora <71889427+Plextora@users.noreply.github.com>
2024-09-30 19:38:37 -0400

    Add mediaSession support

--- src/main/router/resource-router.ts
+++ src/main/router/resource-router.ts
@@ -1,8 +1,9 @@
-import { Router } from '../lib/route-pass/Router';
-import { Storage } from '../lib/storage/Storage';
-import { fail, ok } from '../lib/rust-like-utils-backend/Result';
-
-
+import { Router } from "../lib/route-pass/Router";
+import { Storage } from "../lib/storage/Storage";
+import { none, some } from "../lib/rust-like-utils-backend/Optional";
+import { fail, ok } from "../lib/rust-like-utils-backend/Result";
+import path from "path";
+import sharp from "sharp";
 
 Router.respond("resource::getPath", (_evt, id) => {
   if (id === undefined) {
@@ -18,16 +19,30 @@ Router.respond("resource::getPath", (_evt, id) => {
   return ok(osuDir.value + "/" + encodeFile(id));
 });
 
+Router.respond("resource::getMediaSessionImage", async (_evt, bgPath) => {
+  const settings = Storage.getTable("settings");
+  const songsDir = settings.get("osuSongsDir");
+  if (songsDir.isNone) {
+    return none();
+  }
 
+  const pathToBg = path.join(songsDir.value, bgPath);
+  if (pathToBg === undefined) {
+    // handle no bg
+    return none();
+  }
+  const mimeType = `image/${path.extname(pathToBg).slice(1)}`;
+  const buffer = await sharp(pathToBg).resize(400, 400).toBuffer();
+
+  return some(`data:${mimeType};base64,${buffer.toString("base64")}`);
+});
 
 function encodeFile(uri: string): string {
   return uri
     .split(/[\/\\]/)
-    .map(s => encodeURIComponent(s))
+    .map((s) => encodeURIComponent(s))
     .join("/");
 }
-
-
 
 Router.respond("resource::get", (_evt, id, table) => {
   return Storage.getTable(table).get(id);
--- src/renderer/src/lib/Music.ts
+++ src/renderer/src/lib/Music.ts
@@ -1,64 +1,43 @@
 import { none, some } from "./rust-like-utils-client/Optional.js";
-import { AudioSource, Optional, Song } from '../../../@types';
-import { createDefaultSong, isSongUndefined, msToBPM } from './song';
-import { createEffect, createSignal } from 'solid-js';
-import { delay } from './delay';
-import { createStore } from 'solid-js/store';
-
-
+import { AudioSource, Optional, Song } from "../../../@types";
+import { createDefaultSong, isSongUndefined, msToBPM } from "./song";
+import { createEffect, createSignal } from "solid-js";
+import { delay } from "./delay";
+import { createStore } from "solid-js/store";
 
 type ZeroToOne = number;
 
-
-
 const player = new Audio();
-
-
 
 const [media, setMedia] = createSignal<URL>();
 
-
-
 const [song, setSong] = createStore<Song>(createDefaultSong());
-export { song }
-
-
+export { song };
 
 const [duration, setDuration] = createSignal(0);
-export { duration }
-
-
+export { duration };
 
 const [timestamp, setTimestamp] = createSignal(0);
-export { timestamp }
-
-
+export { timestamp };
 
 const [volume, setVolume] = createSignal<ZeroToOne>(0.3);
-export { volume, setVolume }
-window.api.request("settings::get", "volume")
-  .then(v => {
-    if (v.isNone) {
-      return;
-    }
+export { volume, setVolume };
+window.api.request("settings::get", "volume").then((v) => {
+  if (v.isNone) {
+    return;
+  }
 
-    setVolume(v.value);
-  });
-
-
+  setVolume(v.value);
+});
 
 const [localVolume, setLocalVolume] = createSignal<ZeroToOne>(0.5);
-export { localVolume, setLocalVolume }
-
-
+export { localVolume, setLocalVolume };
 
 function calculateVolume(): number {
   const v = volume();
-  return v + ((localVolume() - 0.5) * 2 * v);
+  return v + (localVolume() - 0.5) * 2 * v;
 }
 player.volume = calculateVolume();
-
-
 
 const [bpm, setBPM] = createSignal<Optional<number>>(none(), {
   equals: (prev, next) => {
@@ -77,16 +56,12 @@ const [bpm, setBPM] = createSignal<Optional<number>>(none(), {
     return true;
   }
 });
-export { bpm }
-
-
+export { bpm };
 
 const [isPlaying, setIsPlaying] = createSignal<boolean>(false);
-export { isPlaying }
+export { isPlaying };
 
-
-
-async function getCurrent(): Promise<{ song: Song, media: URL } | undefined> {
+async function getCurrent(): Promise<{ song: Song; media: URL } | undefined> {
   const song = await window.api.request("queue::current");
 
   if (song.isNone) {
@@ -128,8 +103,7 @@ export async function play(): Promise<void> {
 
   player.volume = calculateVolume();
 
-  await player.play()
-    .catch(reason => console.error(reason));
+  await player.play().catch((reason) => console.error(reason));
 
   setIsPlaying(true);
 }
@@ -156,6 +130,105 @@ export async function next() {
   }
 
   setSong(current.song);
+  await setMediaSessionMetadata();
+  setMediaSessionPosition();
+}
+
+export async function setMediaSession(song: Song) {
+  const bgPath = await window.api.request("resource::getMediaSessionImage", song.bg!);
+  if (bgPath.isNone) {
+    return;
+  }
+
+  if ("mediaSession" in navigator) {
+    await setMediaSessionMetadata();
+    setMediaSessionPosition();
+
+    const actionHandlers = [
+      [
+        "play",
+        () => {
+          togglePlay();
+        }
+      ],
+      [
+        "pause",
+        () => {
+          pause();
+        }
+      ],
+      [
+        "previoustrack",
+        () => {
+          previous();
+        }
+      ],
+      [
+        "nexttrack",
+        () => {
+          next();
+        }
+      ]
+    ];
+
+    for (const [action, handler] of actionHandlers) {
+      try {
+        navigator.mediaSession.setActionHandler(
+          action as MediaSessionAction,
+          handler as MediaSessionActionHandler
+        );
+      } catch (err) {
+        console.log(`The media session action "${action}" is not supported yet.`);
+      }
+    }
+  }
+}
+
+async function setMediaSessionMetadata() {
+  if ("metadata" in navigator.mediaSession) {
+    const bgPath = await window.api.request("resource::getMediaSessionImage", song.bg!);
+    if (bgPath.isNone) {
+      return;
+    }
+
+    if (navigator.mediaSession.metadata !== null) {
+      navigator.mediaSession.metadata.title = song.title;
+      navigator.mediaSession.metadata.artist = song.artist;
+      navigator.mediaSession.metadata.artwork = [
+        // just apply every size and pray that an image shows correctly Pepega
+        { src: bgPath.value, sizes: "96x96", type: "image/png" },
+        { src: bgPath.value, sizes: "128x128", type: "image/png" },
+        { src: bgPath.value, sizes: "192x192", type: "image/png" },
+        { src: bgPath.value, sizes: "256x256", type: "image/png" },
+        { src: bgPath.value, sizes: "384x384", type: "image/png" },
+        { src: bgPath.value, sizes: "512x512", type: "image/png" }
+      ];
+    } else {
+      navigator.mediaSession.metadata = new MediaMetadata({
+        title: song.title,
+        artist: song.artist,
+        artwork: [
+          // just apply every size and pray that an image shows correctly Pepega
+          { src: bgPath.value, sizes: "96x96", type: "image/png" },
+          { src: bgPath.value, sizes: "128x128", type: "image/png" },
+          { src: bgPath.value, sizes: "192x192", type: "image/png" },
+          { src: bgPath.value, sizes: "256x256", type: "image/png" },
+          { src: bgPath.value, sizes: "384x384", type: "image/png" },
+          { src: bgPath.value, sizes: "512x512", type: "image/png" }
+        ]
+      });
+    }
+  }
+}
+
+function setMediaSessionPosition() {
+  if ("setPositionState" in navigator.mediaSession) {
+    navigator.mediaSession.setPositionState({
+      duration: song.duration,
+      playbackRate: player.playbackRate,
+      position: player.currentTime
+    });
+  }
 }
 
 export async function previous() {
@@ -175,6 +248,8 @@ export async function previous() {
   }
 
   setSong(current.song);
+  await setMediaSessionMetadata();
+  setMediaSessionPosition();
 }
 
 export async function togglePlay(force?: boolean): Promise<void> {
@@ -207,12 +282,14 @@ export function seek(range: ZeroToOne): void {
   setTimestamp(player.currentTime);
 }
 
-
-
 createEffect(async () => {
   setBPM(none());
 
-  const audio = await window.api.request("resource::get", song.audio, "audio") as Optional<AudioSource>;
+  const audio = (await window.api.request(
+    "resource::get",
+    song.audio,
+    "audio"
+  )) as Optional<AudioSource>;
 
   if (audio.isNone) {
     return;
@@ -221,23 +298,17 @@ createEffect(async () => {
   setLocalVolume(audio.value.volume ?? 0.5);
 });
 
-
-
 createEffect(() => {
   player.volume = calculateVolume();
 });
 
-
-
-const [writeVolume, ] = delay(async (volume: number) => {
+const [writeVolume] = delay(async (volume: number) => {
   await window.api.request("settings::write", "volume", volume);
 }, 200);
 createEffect(async () => {
   const v = volume();
   writeVolume(v);
 });
-
-
 
 createEffect(async () => {
   const lv = localVolume();
@@ -246,7 +317,11 @@ createEffect(async () => {
     return;
   }
 
-  const audio = await window.api.request("resource::get", song.audio, "audio") as Optional<AudioSource>;
+  const audio = (await window.api.request(
+    "resource::get",
+    song.audio,
+    "audio"
+  )) as Optional<AudioSource>;
 
   if (!audio.isNone && audio.value.volume === lv) {
     return;
@@ -254,8 +329,6 @@ createEffect(async () => {
 
   await window.api.request("save::localVolume", lv, song.path);
 });
-
-
 
 window.api.listen("queue::songChanged", async (s) => {
   const resource = await window.api.request("resource::getPath", s.audio);
@@ -269,10 +342,8 @@ window.api.listen("queue::songChanged", async (s) => {
   await play();
 });
 
-
-
 player.addEventListener("ended", async () => {
-    await next();
+  await next();
 });
 
 const OFFSET = 0;
@@ -294,18 +365,16 @@ player.addEventListener("timeupdate", () => {
   }
 });
 
-
-
 function currentBPM(offset: number, changes: number[][]): Optional<number> {
-    if (changes.length === 0 || offset < changes[0][OFFSET] / 1000) {
-        return none();
-    }
+  if (changes.length === 0 || offset < changes[0][OFFSET] / 1000) {
+    return none();
+  }
 
-    for (let i = 1; i < changes.length; i++) {
-        if (changes[0][OFFSET] / 1000 <= offset && offset < changes[i][OFFSET] / 1000) {
-            return some(msToBPM(changes[i - 1][BPM]));
-        }
+  for (let i = 1; i < changes.length; i++) {
+    if (changes[0][OFFSET] / 1000 <= offset && offset < changes[i][OFFSET] / 1000) {
+      return some(msToBPM(changes[i - 1][BPM]));
     }
+  }
 
-    return some(msToBPM(changes[changes.length - 1][BPM]));
+  return some(msToBPM(changes[changes.length - 1][BPM]));
 }
