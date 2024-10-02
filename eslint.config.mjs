b7c094ddf80af8fc8c9f5c840db157a27ef7cb4b
xDalete <xdalete@proton.me>
2024-10-02 15:55:38 -0300

    Updating libraries + Requested changes

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default [
+  {
+    ignores: ["**/out", "**/build", "**/dist"]
+  },
+  ...compat.extends("plugin:@typescript-eslint/recommended"),
+  {
+    plugins: {
+      "@typescript-eslint": typescriptEslint
+    },
+
+    languageOptions: {
+      parser: tsParser
+    }
+  },
+  {
+    files: ["**/*"],
+
+    rules: {
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/ban-types": "off"
+    }
+  }
+];
