99fc006f502cfc7d03946c9c77b3651bcd5728d0
D0m1nos <21263344+D0m1nos@users.noreply.github.com>
2024-10-11 11:19:41 +0200

    fix app hanging on start

--- src/main/index.ts
+++ src/main/index.ts
@@ -17,7 +17,7 @@ async function createWindow() {
     autoHideMenuBar: true,
     ...(process.platform === "linux" ? { icon } : {}),
     webPreferences: {
-      preload: join(__dirname, "../preload/index.js"),
+      preload: join(__dirname, "../preload/index.mjs"),
       sandbox: false,
       webSecurity: false,
     },
