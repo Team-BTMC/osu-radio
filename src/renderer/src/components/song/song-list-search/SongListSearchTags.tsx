cf3cab18562767f5f45a5a9dd4ab45207d4f5643
dudubtw <carloseduardo108090@hotmail.com>
2024-10-12 13:28:11 -0300

    Dismiss tags search hint

--- src/renderer/src/components/song/song-list-search/SongListSearchTags.tsx
+++ src/renderer/src/components/song/song-list-search/SongListSearchTags.tsx
@@ -1,10 +1,11 @@
 import Dropdown from "@renderer/components/dropdown/Dropdown";
-import { createSignal, For } from "solid-js";
+import { createSignal, For, Show } from "solid-js";
 
 const SongListSearchTags = () => {
   const [hasFetchedTags, setHasFetchedTags] = createSignal(false);
   const [isPopopOpen, setIsPopopOpen] = createSignal(false);
   const [tags, setTags] = createSignal<string[]>([]);
+  const [showHint, setShowHint] = createSignal(true);
 
   const fetchTags = async () => {
     setHasFetchedTags(true);
@@ -27,10 +28,15 @@ const SongListSearchTags = () => {
       <Dropdown.Trigger>Tags</Dropdown.Trigger>
       <Dropdown.Content>
         <div class="song-list-search-tags">
-          <p class="song-list-search-tags__hint">
-            ⓘ Click on any tag once to include it. Click on it again to exclude it. Click once more
-            to clear it
-          </p>
+          <Show when={showHint()}>
+            <div class="song-list-search-tags__hint">
+              <span>
+                ⓘ Click on any tag once to include it. Click on it again to exclude it. Click once
+                more to clear it
+              </span>{" "}
+              <button onClick={() => setShowHint(false)}>Dismiss</button>
+            </div>
+          </Show>
           <div class="song-list-search-tags__content">
             <For each={tags()}>
               {(tag) => <button class="song-list-search-tags__tag">{tag}</button>}
