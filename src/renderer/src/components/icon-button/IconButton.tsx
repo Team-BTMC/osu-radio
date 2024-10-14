dc3ef82aa34eab6caa660f39ba3fc5add19ca6cc
Tnixc <tnixxc@gmail.com>
2024-10-14 13:09:58 -0400

    remove unused css and move some more stuff to tailwind

--- src/renderer/src/components/icon-button/IconButton.tsx
+++ src/renderer/src/components/icon-button/IconButton.tsx
@@ -1,9 +1,13 @@
-import "./styles.css";
 import { Component, JSX } from "solid-js";
 
 type Props = JSX.IntrinsicElements["button"];
 const IconButton: Component<Props> = (props) => {
-  return <button {...props} class={`icon-button ${props.class}`} />;
+  return (
+    <button
+      {...props}
+      class={`-m-2 flex h-6 w-6 items-center justify-center rounded-full p-2 text-2xl hover:bg-surface focus:bg-surface ${props.class}`}
+    />
+  );
 };
 
 export default IconButton;
