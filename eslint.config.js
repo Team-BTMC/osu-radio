b44ab28b6174a8f37e131e08278aed12d411fd94
Bas van den Wollenberg <hi@bas.sh>
2024-10-16 19:39:36 +0200

    add Solid eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import eslint from "@eslint/js";
+import solid from "eslint-plugin-solid";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -12,5 +13,9 @@ export default tseslint.config(
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
     },
+  },
+  {
+    files: ["src/renderer/**/*.tsx"],
+    ...solid.configs["flat/recommended"],
   },
 );
