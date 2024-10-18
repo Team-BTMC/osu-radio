3a3b7701d7428418c43f4919a542d2951b80a5ba
Bas van den Wollenberg <hi@bas.sh>
2024-10-18 03:16:40 +0200

    add package.json key sorting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,9 @@
 /** @type {import("prettier").Options} */
 export default {
   printWidth: 100,
-  plugins: ["@trivago/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+  plugins: [
+    "@trivago/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+    "prettier-plugin-packagejson",
+  ],
 };
