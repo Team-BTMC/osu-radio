2060ddc4106883c743cec3d2b61c449cd84f2c0c
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-10-21 21:28:34 -0700

    typo in drizzle config 🤦‍♀️

--- drizzle/drizzle.config.ts
+++ drizzle/drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./src/man/lib/storage/schema.ts",
+  schema: "./src/main/lib/storage/schema.ts",
   out: "./drizzle",
   dbCredentials: {
     url: process.env.DB_FILE_NAME!,
