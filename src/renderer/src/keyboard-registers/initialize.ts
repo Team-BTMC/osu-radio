836e6be8927d6584db385ef5fbcfd59eb44218dc
aychar <58487401+hrfarmer@users.noreply.github.com>
2024-10-24 19:40:57 -0500

    change keybinds

--- src/renderer/src/keyboard-registers/initialize.ts
+++ src/renderer/src/keyboard-registers/initialize.ts
@@ -1,8 +1,7 @@
 import { Keyboard } from "../lib/Keyboard";
 import "./c-play-pause";
-import "./cmd-s-toggle-sidebar";
-import "./ctrl-s-toggle.sidebar";
 import "./f2-shuffle";
+import "./toggle-sidebar";
 import "./x-play-again";
 
 Keyboard.init();
--- src/renderer/src/keyboard-registers/toggle-sidebar.ts
+++ src/renderer/src/keyboard-registers/toggle-sidebar.ts
@@ -2,8 +2,16 @@ import { Keyboard } from "../lib/Keyboard";
 import { setActive } from "@renderer/scenes/main-scene/MainScene";
 
 Keyboard.register({
-  key: "s",
+  key: "b",
   modifiers: ["cmd"],
+  onPress: () => {
+    setActive((a) => !a);
+  },
+});
+
+Keyboard.register({
+  key: "b",
+  modifiers: ["ctrl"],
   onPress: () => {
     setActive((a) => !a);
   },
