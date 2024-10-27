8d27ec48a9c492607f08cc5231076a825b416d7b
Tnixc <tnixxc@gmail.com>
2024-10-27 17:42:40 -0400

    set up path aliases

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,19 +3,23 @@ import { resolve } from "path";
 import lucidePreprocess from "vite-plugin-lucide-preprocess";
 import solid from "vite-plugin-solid";
 
+const alias = {
+  "@": resolve("src"),
+  "@types": resolve("src/@types.d.ts"),
+  "@main": resolve("src/main"),
+  "@renderer": resolve("src/renderer/src"),
+};
+
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()],
+    resolve: { alias },
   },
   preload: {
     plugins: [externalizeDepsPlugin()],
   },
   renderer: {
-    resolve: {
-      alias: {
-        "@renderer": resolve("src/renderer/src"),
-      },
-    },
+    resolve: { alias },
     plugins: [lucidePreprocess(), solid()],
   },
 });
