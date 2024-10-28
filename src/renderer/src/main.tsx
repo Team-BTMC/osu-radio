f959aab89c2eca9f2caad84d1b60554123953dcb
Bas van den Wollenberg <hi@bas.sh>
2024-10-28 22:12:43 +0100

    add solid-devtools

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,5 +2,8 @@ import App from "./App";
 import "@renderer/css/global.css";
 import "@renderer/css/theme.css";
 import { render } from "solid-js/web";
+import { attachDevtoolsOverlay } from "@solid-devtools/overlay";
+
+attachDevtoolsOverlay();
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
