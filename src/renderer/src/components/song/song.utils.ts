5ee0459747ad508b6caec8cf17c4438cda057e70
aychar <58487401+hrfarmer@users.noreply.github.com>
2025-01-27 14:19:03 -0600

    fix type errors

--- src/renderer/src/components/song/song.utils.ts
+++ src/renderer/src/components/song/song.utils.ts
@@ -1,26 +1,13 @@
 import { delay } from "@renderer/lib/delay";
 import { none, some } from "@renderer/lib/rust-like-utils-client/Optional";
-import { isSongUndefined, msToBPM } from "@renderer/lib/song";
+import { createDefaultSong, isSongUndefined, msToBPM } from "@renderer/lib/song";
 import { createEffect, createSignal } from "solid-js";
 import { AudioSource, Optional, Song } from "src/@types";
 
 /** Range from 0 to 1. */
 export type ZeroToOne = number;
 
-const DEFAULT_SONG: Song = {
-  dateAdded: "",
-  ctime: "",
-  path: "",
-  audio: "",
-  bg: "",
-  osuFile: "",
-  title: "",
-  artist: "",
-  creator: "",
-  duration: 0,
-  bpm: [],
-  diffs: [],
-};
+const DEFAULT_SONG = createDefaultSong();
 
 // ------------
 // State
